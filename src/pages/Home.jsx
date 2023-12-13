8f8691b548786cf7b1170986f57e58ab5fad7c27
Ravan Gozalov <ravangozalov@Ravans-Air.lan>
2023-12-13 23:31:44 +0400

    creating json

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to the Flash Card App</h1>
+      <ul>
+        <li>Question 1</li>
+        <li>Question 2</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Home;
