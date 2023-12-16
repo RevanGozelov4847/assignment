ff16bd9684d937e938b825942c69e31f6beea6e9
Ravan Gozalov <ravangozalov@Ravans-Air.lan>
2023-12-17 01:53:51 +0400

    edit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-    <Home/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
