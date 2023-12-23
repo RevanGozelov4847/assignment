46a3c0a01687bbe6da69620a275b15193e8dc25f
Ravan Gozalov <ravangozalov@Ravans-Air.lan>
2023-12-24 02:07:10 +0400

    edit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import FlashCards from './components/FlashCards';
 import { FlashCardsProvider } from './context/FlashCardsContext';
-import "./assets/scss/main.scss"
+import Navbar from './components/Navbar'; // Import the Navbar component
+import './assets/scss/main.scss';
 
 const App = () => {
   return (
     <Router>
       <FlashCardsProvider>
+        <Navbar /> {/* Include the Navbar component */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/flash-cards" element={<FlashCards />} />
--- src/components/FlashCards.jsx
+++ src/components/FlashCards.jsx
@@ -1,10 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import CardFormModal from "./CardFormModal";
 import { useFlashCardsContext } from "../context/FlashCardsContext";
 
 const FlashCards = () => {
-  const { flashCards, addFlashCard, updateFlashCard, deleteFlashCard } =
-    useFlashCardsContext();
+  const {
+    flashCards,
+    setFlashCards,
+    addFlashCard,
+    updateFlashCard,
+    deleteFlashCard,
+  } = useFlashCardsContext();
   const [newCard, setNewCard] = useState({
     id: null,
     question: "",
@@ -14,6 +19,16 @@ const FlashCards = () => {
   const [isModalOpen, setModalOpen] = useState(false);
   const [flippedCards, setFlippedCards] = useState({});
   const [selectedCard, setSelectedCard] = useState(null);
+  const [searchText, setSearchText] = useState("");
+  const [filterStatus, setFilterStatus] = useState("All");
+  const [sortOption, setSortOption] = useState("Last Modified");
+
+  useEffect(() => {
+    fetch('http://localhost:3001/flashCards')
+      .then((response) => response.json())
+      .then((data) => setFlashCards(data))
+      .catch((error) => console.error('Error fetching flash cards:', error));
+  }, [setFlashCards]); 
 
   const onSave = () => {
     console.log("Card saved!");
@@ -62,13 +77,61 @@ const FlashCards = () => {
     setSelectedCard(cardId);
   };
 
+  const getSortedCards = () => {
+    if (sortOption === "Last Modified") {
+      return [...flashCards].sort((a, b) =>
+        new Date(b.lastModified) - new Date(a.lastModified)
+      );
+    } else if (sortOption === "Alphabetical order") {
+      return [...flashCards].sort((a, b) => a.question.localeCompare(b.question));
+    } else {
+      return flashCards;
+    }
+  };
+
+  const filteredAndSortedCards = getSortedCards()
+    .filter(
+      (card) =>
+        card.question.toLowerCase().includes(searchText.toLowerCase()) ||
+        card.answer.toLowerCase().includes(searchText.toLowerCase())
+    )
+    .filter(
+      (card) =>
+        filterStatus === "All" || card.status.toLowerCase() === filterStatus.toLowerCase()
+    );
+
   return (
     <div>
       <h1>Flash Cards</h1>
       <button onClick={() => setModalOpen(true)}>Create New Card</button>
 
+      <div>
+        <input
+          type="text"
+          placeholder="Search..."
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+        <select
+          value={filterStatus}
+          onChange={(e) => setFilterStatus(e.target.value)}
+        >
+          <option value="All">All</option>
+          <option value="Learned">Learned</option>
+          <option value="Want to Learn">Want to Learn</option>
+          <option value="Noted">Noted</option>
+        </select>
+        <select
+          value={sortOption}
+          onChange={(e) => setSortOption(e.target.value)}
+        >
+          <option value="Last Modified">Last Modified</option>
+          <option value="Alphabetical order">Alphabetical order</option>
+        </select>
+      </div>
+
       <div className="flash-cards-container">
-        {flashCards.map((card) => (
+        {filteredAndSortedCards.map((card) => (
           <div key={card.id} className="flash-card-container">
             <div
               className={`flash-card ${flippedCards[card.id] ? "flipped" : ""}`}
@@ -89,26 +152,26 @@ const FlashCards = () => {
                     Last Modified: {new Date(card.lastModified).toLocaleString()}
                   </div>
                 </div>
+                {selectedCard === card.id && (
+                  <div className="card-actions">
+                    <button
+                      type="button"
+                      className="edit-btn"
+                      onClick={() => handleEditCard(card)}
+                    >
+                      Edit Card
+                    </button>
+                    <button
+                      type="button"
+                      className="delete-btn"
+                      onClick={() => handleDeleteCard(card.id)}
+                    >
+                      Delete Card
+                    </button>
+                  </div>
+                )}
               </div>
             </div>
-            {selectedCard === card.id && (
-              <div className="card-actions">
-                <button
-                  type="button"
-                  className="edit-btn"
-                  onClick={() => handleEditCard(card)}
-                >
-                  Edit Card
-                </button>
-                <button
-                  type="button"
-                  className="delete-btn"
-                  onClick={() => handleDeleteCard(card.id)}
-                >
-                  Delete Card
-                </button>
-              </div>
-            )}
           </div>
         ))}
       </div>
